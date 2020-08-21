443fc15242417bb7c988650e7e5fd18ca27d9047
Akhila Nekkanti <akhilan@uoregon.edu>
2020-08-21 12:55:14 -0700

    updated loops with Rene's input

--- Preprocessing_loop.m
+++ Preprocessing_loop.m
@@ -1,4 +1,4 @@
-%% Script from Rene on 8/7/2020
+%% Script from Rene Huster on 8/7/2020 - does not include channel locations
 
 [file, path, indx] = uigetfile( '*.raw' , 'Select One or More Files' , 'MultiSelect' , 'on' );
 
@@ -8,12 +8,8 @@ for s = 1:size(file,2)
 
     EEG = pop_eegfiltnew(EEG, 0.1); 
 
-    %akhila adds --for 256 channel nets include:
-    %EEG=pop_chanedit(EEG, 'lookup','C:\\Users\\acer\\Desktop\\CAPS_EEG\\electrode_locations\\GSN-HydroCel-64_1.0.sfp','filetype' 'sfp' 'eval','','rplurchanloc',1)
-
-    % automatic detection of bad channels 
     [throwAway badChannels] = pop_rejchan(EEG, 'elec' ,[1:64] , 'threshold' ,5, 'norm' , 'on' , 'measure' , 'kurt' );
-    % spherical interpolation of bad channels 
+    
     EEG = pop_interp(EEG, badChannels, 'spherical' ); 
 
     EEG = pop_reref( EEG, []); 
@@ -30,3 +26,5 @@ for s = 1:size(file,2)
 
     STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[]; 
 end 
+
+
