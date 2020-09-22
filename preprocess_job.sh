888fa7a7c9465a6a7af6033d0d314e7ff82dd28e
Akhila Nekkanti <akhilan@uoregon.edu>
2020-09-21 18:18:14 -0700

    unicode, reorg, add test file

--- preprocess_job.sh
+++ preprocess_job.sh
@@ -13,14 +13,12 @@ SINGLECOREMATLAB=true
 ADDITIONALOPTIONS=""
 
 if "$SINGLECOREMATLAB"; then
-	ADDITIONALOPTIONS="-singleCompThread"
+ ADDITIONALOPTIONS="-singleCompThread"
 fi
 
 # create and execute job
-echo -------------------------------------------------------------------------------
 echo "${SUB}"
 echo "Running ${SCRIPT}"
-echo -------------------------------------------------------------------------------
 
 module load matlab
 matlab -nosplash -nodisplay -nodesktop ${ADDITIONALOPTIONS} -r "clear; addpath('$EEGLAB_PATH'); sub='$SUB'; script_file='$SCRIPT'; run('preprocess_ct.m'); exit"
